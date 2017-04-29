c666ba7862f0915c13aab2d77912292c6b6ea1d0
Rodolfo Montes <rodolfo.montesgarza@gmail.com>
2017-04-29 11:59:43 -0500

    Add: SignUpActivity and actionbar_toolbar

---
+++ app/src/main/java/com/example/rodolfo/materialdesign/views/SignUpActivity.java
@@ -0,0 +1,19 @@
+package com.example.rodolfo.materialdesign.views;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.rodolfo.materialdesign.R;
+
+/**
+ * Created by rodolfomg on 29/04/17.
+ */
+
+public class SignUpActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
